4ab5765b7acaa29e8690d904125eadcc85a67c72
dkulik <kulikdenis1996x@gmail.com>
2025-03-22 20:54:19 +0700

    feat: add lesson 4

---
+++ 04-props/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Number from '@/components/DisplayNumber.vue'
+
+const count = ref<number>(0)
+</script>
+
+<template>
+  <Number :count class="foo" />
+  <button @click="count++">+</button>
+  <button @click="count--">-</button>
+</template>
---
+++ 04-props/src/components/DisplayNumber.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { watch, useAttrs } from 'vue'
+
+const { count = 0 } = defineProps<{
+  count?: number
+}>()
+
+defineOptions({
+  inheritAttrs: false
+})
+
+const attrs = useAttrs()
+
+watch(() => count, (newValue, oldValue) => {
+  if (oldValue < 10 && newValue >= 10) {
+    console.log('10이상')
+  }
+})
+</script>
+
+<template>
+  <div class="wrapper">
+    <h3>Current value</h3>
+    <div :style="{
+      fontSize: Math.max(count * 5, 5) + 'px'
+    }" v-bind="attrs">
+      {{ count }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  border: 1px solid #000;
+}
+.foo {
+  color: red
+}
+</style>
